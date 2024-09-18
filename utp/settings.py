1970d6b3a0632555237817849133e33df048a6be
Miguel Soto <sistemas@rgd.com.co>
2024-09-18 11:43:29 -0500

    se configura la BD PostgreSQL

--- utp/settings.py
+++ utp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'utp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vivero', 
+        'USER': 'postgres',
+        'PASSWORD': 'TRAP:/ov7Hjlp/u',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
 
