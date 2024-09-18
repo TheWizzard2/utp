e51a8171fc7dfb5332f8b1724edd0552382a11f7
Miguel Soto <sistemas@rgd.com.co>
2024-09-18 11:51:08 -0500

    Se crea el modelo Productor

--- vivero/models.py
+++ vivero/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Modelo Productor
+class Productor(models.Model):
+    documento_identidad = models.CharField(max_length=50, unique=True, null=False)
+    nombre = models.CharField(max_length=100, null=False)
+    apellido = models.CharField(max_length=100, null=False)
+    telefono = models.CharField(max_length=15, null=False)
+    correo = models.EmailField(null=False)
+
+    # Método toString cuando se necesite ver el objeto instanciado
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
