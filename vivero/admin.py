e7f951778c557ef59fb1412130c32b4f54b92d4e
Miguel Soto <sistemas@rgd.com.co>
2024-09-18 13:18:35 -0500

    se registran los modelos en el admin, para poder crear datos

--- vivero/admin.py
+++ vivero/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from vivero.models import *
+
 # Register your models here.
+admin.site.register(Productor)
+admin.site.register(Finca)
+admin.site.register(Vivero)
+admin.site.register(Labor)
+admin.site.register(ProductoControlHongo)
+admin.site.register(ProductoControlPlaga)
+admin.site.register(ProductoControlFertilizante)
+admin.site.register(LaborProductoControl)
