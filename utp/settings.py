6ce4caf790f9c218676963f7233d19448b41f08a
Miguel Soto <sistemas@rgd.com.co>
2024-09-18 11:45:28 -0500

    se añade la aplicación vivero al proyecto

--- utp/settings.py
+++ utp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'vivero.apps.ViveroConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
